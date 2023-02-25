5f1a83ec228d73ad881bf7e8693a16f00c081ad4
Thomas Vincent <thomasvincent@gmail.com>
2023-02-25 02:01:34 -0800

    Update mail.py

--- mail.py
+++ mail.py
@@ -1,62 +1,80 @@
+import os
 import smtplib
 import mimetypes
 from email.mime.multipart import MIMEMultipart
-from email import encoders
-from email.message import Message
-from email.mime.audio import MIMEAudio
-from email.mime.base import MIMEBase
-from email.mime.image import MIMEImage
 from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+
+class EmailSender:
+    def __init__(self, email_from, email_to, username, password, smtp_server):
+        self.email_from = email_from
+        self.email_to = email_to
+        self.username = username
+        self.password = password
+        self.smtp_server = smtp_server
+
+    def send_email(self, file_to_send, subject):
+        if not os.path.isfile(file_to_send):
+            raise ValueError('Invalid file path')
+
+        if not self.email_from:
+            raise ValueError('Missing sender email')
+
+        if not self.email_to:
+            raise ValueError('Missing recipient email')
+
+        if not self.username:
+            raise ValueError('Missing email username')
+
+        if not self.password:
+            raise ValueError('Missing email password')
+
+        if not self.smtp_server:
+            raise ValueError('Missing SMTP server information')
+
+        msg = MIMEMultipart()
+        msg["From"] = self.email_from
+        msg["To"] = self.email_to
+        msg["Subject"] = subject
+        msg.preamble = subject
+
+        text_part = MIMEText('Check for zenoss devices in attachment, please see it', 'plain')
+        msg.attach(text_part)
+
+        ctype, encoding = mimetypes.guess_type(file_to_send)
+        if ctype is None or encoding is not None:
+            ctype = "application/octet-stream"
+
+        maintype, subtype = ctype.split("/", 1)
+
+        with open(file_to_send, 'rb') as fp:
+            if maintype == "text":
+                attachment = MIMEText(fp.read(), _subtype=subtype)
+            elif maintype == "image":
+                attachment = MIMEImage(fp.read(), _subtype=subtype)
+            elif maintype == "audio":
+                attachment = MIMEAudio(fp.read(), _subtype=subtype)
+            else:
+                attachment = MIMEBase(maintype, subtype)
+                attachment.set_payload(fp.read())
+                encoders.encode_base64(attachment)
+
+        attachment.add_header("Content-Disposition", "attachment", filename=file_to_send)
+        msg.attach(attachment)
+
+        with smtplib.SMTP(self.smtp_server) as server:
+            server.starttls()
+            server.login(self.username, self.password)
+            server.sendmail(self.email_from, self.email_to, msg.as_string())
 
 
-EMAILFROM = 'user@gmail.com'
-EMAILTO = 'user@icloud.com'
-USERNAME = 'user@gmail.com'
-PASSWORD = 'password'
-SMTP_SERVER = 'smtp.gmail.com:587'
+if __name__ == '__main__':
+    email_sender = EmailSender(email_from='user@gmail.com',
+                               email_to='user@icloud.com',
+                               username='user@gmail.com',
+                               password='password',
+                               smtp_server='smtp.gmail.com:587')
 
-
-def send(fileToSend):
-    msg = MIMEMultipart()
-    msg["From"] = EMAILFROM
-    msg["To"] = EMAILTO
-    msg["Subject"] = "Check for zenoss devices"
-    msg.preamble = "Check for zenoss devices"
-
-    text_part = MIMEText('Check for zenoss devices in attachment, please see it', 'plain')
-    msg.attach(text_part)
-
-    ctype, encoding = mimetypes.guess_type(fileToSend)
-    if ctype is None or encoding is not None:
-        ctype = "application/octet-stream"
-
-    maintype, subtype = ctype.split("/", 1)
-
-    if maintype == "text":
-        fp = open(fileToSend)
-        # Note: we should handle calculating the charset
-        attachment = MIMEText(fp.read(), _subtype=subtype)
-        fp.close()
-    elif maintype == "image":
-        fp = open(fileToSend, "rb")
-        attachment = MIMEImage(fp.read(), _subtype=subtype)
-        fp.close()
-    elif maintype == "audio":
-        fp = open(fileToSend, "rb")
-        attachment = MIMEAudio(fp.read(), _subtype=subtype)
-        fp.close()
-    else:
-        fp = open(fileToSend, "rb")
-        attachment = MIMEBase(maintype, subtype)
-        attachment.set_payload(fp.read())
-        fp.close()
-        encoders.encode_base64(attachment)
-    attachment.add_header("Content-Disposition", "attachment",
-                          filename=fileToSend)
-    msg.attach(attachment)
-
-    server = smtplib.SMTP(SMTP_SERVER)
-    server.starttls()
-    server.login(USERNAME, PASSWORD)
-    server.sendmail(EMAILFROM, EMAILTO, msg.as_string())
-    server.quit()
+    email_sender.send_email(file_to_send='path/to/file',
+                            subject='Check for zenoss devices')
