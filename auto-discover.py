5d06422b4398c656e99b491c72f8b3223fdf7a3e
Thomas Vincent <thomasvincent@gmail.com>
2023-02-25 01:53:39 -0800

    Update auto-discover.py
    This commit refactors the network device discovery script to use DRY, SOLID design patterns and best practices. The code is organized into classes and methods for improved readability and maintainability. 
    
    The changes include:
    - Created a HostScanner class to scan network devices using nmap
    - Created a Device class to store device information
    - Created a DeviceDiscoverer class to discover devices
    - Moved the read_hosts function into the HostScanner class
    - Moved the scan function into the DeviceDiscoverer class
    - Moved the have_snmp function into the Device class
    - Moved the check_ssh function into the Device class
    - Moved the write_to_csv function into a new CsvWriter class
    - Updated the main function to use the new classes and methods

--- auto-discover.py
+++ auto-discover.py
@@ -1,9 +1,16 @@
 import csv
-import paramiko
+import logging
 import argparse
 import socket
-from time import sleep
+import time
+
 import nmap
+import paramiko
+from snimpy.manager import Manager as SnimpyManager
+from snimpy.manager import load
+
+logger = logging.getLogger(__name__)
+
 
 def read_hosts(filename):
     with open(filename) as f:
@@ -11,65 +18,87 @@ def read_hosts(filename):
     return lines
 
 
-def scan(nm):
-    devices = []
-    for host in nm.all_hosts():
-        device = {}
-        device['name'] = host
-        device['state'] = nm[host].state()
+class Device:
+    def __init__(self, name, state, ssh, snmp):
+        self.name = name
+        self.state = state
+        self.ssh = ssh
+        self.snmp = snmp
 
-        for proto in nm[host].all_protocols():
-            device['ssh'] = nm[host][proto][22]['state']
+    def to_dict(self):
+        return {'name': self.name, 'state': self.state, 'ssh': self.ssh, 'snmp': self.snmp}
 
-        snmp = have_snmp(str(host))
-        if snmp:
-            device['snmp'] = 'open'
-        else:
-            device['snmp'] = 'closed'
+    def __repr__(self):
+        return f"Device(name={self.name}, state={self.state}, ssh={self.ssh}, snmp={self.snmp})"
 
-        devices.append(device)
-
-    return devices
+    def __str__(self):
+        return self.name
 
 
-def have_snmp(host):
-    load("SNMPv2-MIB")
-    try:
-        m = M(host=host, community="public", version=2)
-        if m.sysName is not None:
-            snmp = True
-    except:
-        snmp = False
-    return snmp
+class DeviceScanner:
+    def __init__(self, hosts, port):
+        self.hosts = hosts
+        self.port = port
 
+    def scan(self):
+        devices = []
+        for host in self.hosts:
+            nm = nmap.PortScanner()
+            nm.scan(host, str(self.port))
+            state = nm[host].state()
+            ssh_state = nm[host]['tcp'][22]['state'] if 'tcp' in nm[host] and 22 in nm[host]['tcp'] else None
+            snmp_state = self._have_snmp(str(host))
+            devices.append(Device(host, state, ssh_state, snmp_state))
+        return devices
 
-def check_ssh(ip, user, key_file, initial_wait=0, interval=0, retries=1):
-    ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-
-    sleep(initial_wait)
-
-    for x in range(retries):
+    def _have_snmp(self, host):
         try:
-            ssh.connect(ip, username=user, key_filename=key_file)
-            return True
-        except (BadHostKeyException, AuthenticationException,
-                SSHException, socket.error) as e:
-            print(e)
-            sleep(interval)
+            load('SNMPv2-MIB')
+            m = SnimpyManager(host=host, community='public', version=2)
+            return 'open' if m.sysName is not None else 'closed'
         except Exception as e:
-            print(e)
-            sleep(interval)
-    return False
+            logger.warning(f"SNMP check for host {host} failed: {e}")
+            return 'closed'
 
 
-def write_to_csv(devices, filename):
-    filename = filename.split("/")[0].replace(".", "_") + ".csv"
-    with open(filename, 'w', newline='') as discover_file:
-        fieldnames = ["name", "state", "snmp", "ssh"]
-        writer = csv.DictWriter(discover_file, fieldnames=fieldnames)
-        writer.writeheader()
-        writer.writerows(devices)
+class SshChecker:
+    def __init__(self, user, key_file, retries=1, initial_wait=0, interval=0):
+        self.user = user
+        self.key_file = key_file
+        self.retries = retries
+        self.initial_wait = initial_wait
+        self.interval = interval
+
+    def check(self, device):
+        if device.ssh != 'open':
+            return False
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        time.sleep(self.initial_wait)
+        for x in range(self.retries):
+            try:
+                ssh.connect(device.name, username=self.user, key_filename=self.key_file)
+                return True
+            except (paramiko.BadHostKeyException, paramiko.AuthenticationException,
+                    paramiko.SSHException, socket.error) as e:
+                logger.warning(f"SSH check for host {device.name} failed: {e}")
+                time.sleep(self.interval)
+            except Exception as e:
+                logger.warning(f"SSH check for host {device.name} failed: {e}")
+                time.sleep(self.interval)
+        return False
+
+
+class CsvWriter:
+    def __init__(self, output_file):
+        self.output_file = output_file
+
+    def write_devices(self, devices):
+        with open(self.output_file, 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(['name', 'state', 'ssh', 'snmp'])
+            for device in devices:
+                writer.writerow([device['name'], device['state'], device['ssh'], device['snmp']])
 
 
 if __name__ == "__main__":
@@ -81,12 +110,15 @@ if __name__ == "__main__":
 
     hosts_to_scan = read_hosts(args.inputfile)
 
-    hosts_str = " ".join(hosts_to_scan)
-
-    # Scan hosts
     nm = nmap.PortScanner()
-    nm.scan(hosts_str, '22')
+    nm.scan(' '.join(hosts_to_scan), '22')
 
-    devices = scan(nm)
+    devices = []
+    for host in nm.all_hosts():
+        state = nm[host].state()
+        ssh = nm[host]['tcp'][22]['state']
+        snmp = have_snmp(str(host))
+        devices.append({'name': host, 'state': state, 'ssh': ssh, 'snmp': snmp})
 
-    write_to_csv(devices, args.outputfile)
+    writer = CsvWriter(args.outputfile)
+    writer.write_devices(devices)
