5d1f12f1561143011f1659bc49f0df505729730f
Thomas Vincent <thomasvincent@gmail.com>
2025-05-06 13:48:34 -0700

    Remove Python 3.7 support and update to only use supported Python versions

--- setup.py
+++ setup.py
@@ -65,7 +65,6 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
@@ -77,7 +76,7 @@ setup(
     keywords="nmap portscanner network discovery sysadmin",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
-    python_requires=">=3.7",
+    python_requires=">=3.8",
     install_requires=requirements,
     extras_require={
         "dev": dev_requirements,
